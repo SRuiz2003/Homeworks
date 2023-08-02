d91c04e53a1e7c8a4e94afca0f0d80a86eb200c6
SRuiz2003 <123700454+SRuiz2003@users.noreply.github.com>
2023-08-02 16:30:03 -0500

    Challenge01

---
+++ ArrowVsRegular.js
@@ -0,0 +1,15 @@
+const Arrow = (a) => {
+    if (a%2 == 0) {
+        return "Es par"
+    } else{
+        return "es impar"
+    }
+}
+
+const regular = function(a){
+    if (a%2 == 0) {
+        return "Es par"
+    } else{
+        return "es impar"
+    }
+}
