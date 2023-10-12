b8caed768eb7e2795db7652cf61b44826ade479a
SRuiz2003 <123700454+SRuiz2003@users.noreply.github.com>
2023-10-11 23:02:19 -0500

    Fixed

--- Front/vite-project/src/components/GifGrid.jsx
+++ Front/vite-project/src/components/GifGrid.jsx
@@ -6,10 +6,11 @@ const getGifs = async(category) => {
     const resp = await fetch(url);
     const {data} = await resp.json()
     const gifs = data.map( (img) => {
+        console.log(img);
         return {
             id:img.id,
             title:img.title,
-            url:img.images.downsized_medium.url,
+            url:img.images.fixed_width_downsampled.url,
         }
     });
 
